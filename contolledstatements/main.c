2b71e222dc230124c94a33a88aed7182b3ac2b6e
mallikhushi <mallikhushid28@gmail.com>
2025-07-23 12:09:52 +0530

    main.c

--- contolledstatements/main.c
+++ contolledstatements/main.c
@@ -41,33 +41,3 @@ int main()
 
 
 
-
-#include <stdio.h>
-
-int main() {
-    int m, p, c, total;
-    float avg;
-
-    printf("Enter marks in Maths: ");
-    scanf("%d", &m);
-    printf("Enter marks in Physics: ");
-    scanf("%d", &p);
-    printf("Enter marks in Chemistry: ");
-    scanf("%d", &c);
-
-    total = m + p + c;
-    avg = total / 3.0;  // Float division to get accurate average
-
-    printf("Total Marks = %d\n", total);
-    printf("Average = %.2f\n", avg);
-
-    // Grade based on average
-    if (avg >= 90 && avg <= 100)
-        printf("Grade: A\n");
-    else if (avg >= 80)
-        printf("Grade: B\n");
-    else if (avg >= 70)
-        printf("Grade: C\n");
-    else if (avg >= 60)
-        printf("Grade:
-
