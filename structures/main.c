bc59967426cc19d0aa8e2444198ff068d68f6ea4
mallikhushi <mallikhushid28@gmail.com>
2025-07-23 12:17:22 +0530

    main.c

--- structures/main.c
+++ structures/main.c
@@ -24,17 +24,3 @@ int main()
     return 0;
 }
 
-
-printf("Enter the employee ID: ");
-    scanf("%d", &e.id);
-
-    printf("Enter the years of experience: ");
-    scanf("%f", &e.experience);
-
-    printf("Enter the department: ");
-    scanf(" %[^\n]", e.department);
-
-    printf("\nEmployee Name: %s\n", e.name);
-    printf("Employee ID: %d\n", e.id);
-    printf("Years of Experience: %.2f\n", e.experience);
-    printf("Department: %s\n", e.department);
